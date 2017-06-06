0f3a6cdc4f735563394b3cb63ecd7a7580830c29
Seunghun Lee <waydi1@gmail.com>
2017-06-07 02:00:02 +0900

    Implement updateWeights and resample

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -84,6 +84,24 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
   // NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
   //   implement this method and use it as a helper during the updateWeights phase.
 
+  for (int i = 0; i < observations.size(); i++) {
+    double min_distance = 99999999;
+    for (int j = 0; j < predicted.size(); j++) {
+      double distance = dist(observations[i].x, observations[i].y, predicted[j].x, predicted[j].y);
+
+      if (distance < min_distance) {
+        min_distance = distance;
+        observations[i].id = predicted[j].id;
+      }
+    }
+  }
+}
+
+double multivariate_gaussian(double x, double y, double mu_x, double mu_y, double sig_x, double sig_y) {
+  return exp(-(
+        (pow(x - mu_x, 2) / (2 * pow(sig_x, 2)) +
+         pow(y - mu_y, 2) / (2 * pow(sig_y, 2))
+        ))) / (2 * M_PI * sig_x * sig_y);
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
@@ -98,13 +116,72 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   //   and the following is a good resource for the actual equation to implement (look at equation
   //   3.33
   //   http://planning.cs.uiuc.edu/node99.html
+
+  for (int i = 0; i < num_particles; i++) {
+    Particle p = particles[i];
+    vector<LandmarkObs> transformed_observations;
+
+    for (int j = 0; j < observations.size(); j++) {
+      LandmarkObs obs = observations[j];
+      LandmarkObs transformed;
+
+      transformed.x = obs.x * cos(p.theta) - obs.y * sin(p.theta) + p.x;
+      transformed.y = obs.y * cos(p.theta) + obs.x * sin(p.theta) + p.y;
+      transformed.id = 0;
+
+      transformed_observations.push_back(transformed);
+    }
+
+    vector<LandmarkObs> predicted;
+
+    for (int j = 0; j < map_landmarks.landmark_list.size(); j++) {
+      Map::single_landmark_s l = map_landmarks.landmark_list[j];
+      double distance = dist(p.x, p.y, l.x_f, l.y_f);
+
+      if (distance < sensor_range) {
+        LandmarkObs obs;
+        obs.x = l.x_f;
+        obs.y = l.y_f;
+        obs.id = l.id_i;
+        predicted.push_back(obs);
+      }
+    }
+
+    if (predicted.size() > 0) {
+      dataAssociation(predicted, transformed_observations);
+
+      p.weight = 1;
+
+      for (int j = 0; j < transformed_observations.size(); j++) {
+        LandmarkObs obs = transformed_observations[j];
+        Map::single_landmark_s l = map_landmarks.landmark_list[obs.id - 1];
+        double weight = multivariate_gaussian(
+            l.x_f, l.y_f, obs.x, obs.y, std_landmark[0], std_landmark[1]);
+
+        p.weight *= weight;
+      }
+    } else {
+      p.weight = 0;
+    }
+
+    weights[i] = p.weight;
+    particles[i].weight = p.weight;
+  }
 }
 
 void ParticleFilter::resample() {
   // TODO: Resample particles with replacement with probability proportional to their weight.
   // NOTE: You may find std::discrete_distribution helpful here.
   //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  vector<Particle> new_particles;
+  default_random_engine gen;
+  discrete_distribution<> distribution(weights.begin(), weights.end());
 
+  for (int i = 0; i < num_particles; i++) {
+    int number = distribution(gen);
+    new_particles.push_back(particles[number]);
+  }
+  particles = new_particles;
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)
